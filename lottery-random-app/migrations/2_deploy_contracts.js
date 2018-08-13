62beecbc22809d577bb09d332f08fe3862ffb36a
haoxu <haoxu@onething.net>
2018-08-13 17:30:29 +0800

    add lottery test case

--- lottery-random-app/migrations/2_deploy_contracts.js
+++ lottery-random-app/migrations/2_deploy_contracts.js
@@ -1,17 +1,15 @@
-const LotteryData = artifacts.require("./LotteryData.sol")
-const LotteryCore = artifacts.require("./LotteryCore.sol")
+const LotteryData = artifacts.require("LotteryData")
+const LotteryCore = artifacts.require("LotteryCore")
 
 let lotteryData, lotteryCore
 
 module.exports = function(deployer, nerworks, accounts) {
-  console.info(accounts)
   let owner = accounts[0];
   deployer.deploy(LotteryData, owner).then(res => {
     lotteryData = res
-    deployer.deploy(LotteryCore, owner, lotteryData.address).then(lotteryCoreRes => {
+    return deployer.deploy(LotteryCore, owner, lotteryData.address).then(lotteryCoreRes => {
       lotteryCore = lotteryCoreRes
       lotteryData.setLotteryCore(lotteryCore.address)
-      lotteryCore.addAdmin(owner)
     })
   })
 }
